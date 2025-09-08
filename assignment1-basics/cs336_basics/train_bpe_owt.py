73376540d5b38f7898f38cae02a9a3f4868fe44c
ziliang <ziliangcok@qq.com>
2025-09-08 21:32:28 +0800

    ADD: Training BPE on TinyStories & OpenWebText

--- assignment1-basics/cs336_basics/train_bpe_owt.py
+++ assignment1-basics/cs336_basics/train_bpe_owt.py
@@ -4,18 +4,18 @@ Training BPE tokenizer on the TinyStories datasets
 from bpe import BPETokenizer
 
 if __name__ == "__main__":
-    data_path = "/volume/pt-train/users/wzhang/zzl-workspace/cs336/assignment1-basics/data/TinyStoriesV2-GPT4-train.txt"
+    data_path = "/volume/pt-train/users/wzhang/zzl-workspace/cs336/assignment1-basics/data/owt_train.txt"
     tokenizer = BPETokenizer()
     vocab, merges = tokenizer.train(
         input_path=data_path,
-        vocab_size=10000
+        vocab_size=32000
     )
 
     # Save the vocabulary and merges
-    with open("/volume/pt-train/users/wzhang/zzl-workspace/cs336/assignment1-basics/cs336_basics/ts-vocab.txt", "w") as vocab_file:
+    with open("/volume/pt-train/users/wzhang/zzl-workspace/cs336/assignment1-basics/cs336_basics/owt-vocab.txt", "w") as vocab_file:
         for idx, token in vocab.items():
             vocab_file.write(f"{idx}\t{token}\n")
-    with open("/volume/pt-train/users/wzhang/zzl-workspace/cs336/assignment1-basics/cs336_basics/ts-merges.txt", "w") as merges_file:
+    with open("/volume/pt-train/users/wzhang/zzl-workspace/cs336/assignment1-basics/cs336_basics/owt-merges.txt", "w") as merges_file:
         for pair in merges:
             merges_file.write(f"{pair[0]} {pair[1]}\n")
     
