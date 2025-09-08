01052d0f7c9d495743b38a92dce80479c4e3d8a9
ziliang <ziliangcok@qq.com>
2025-09-08 21:34:14 +0800

    ADD: Encode & Decode function (without test)

--- assignment1-basics/cs336_basics/bpe.py
+++ assignment1-basics/cs336_basics/bpe.py
@@ -8,10 +8,10 @@ import os
 import regex as re
 import logging
 from collections import defaultdict, Counter
-from typing import BinaryIO
+from typing import BinaryIO, Iterable, Iterator
 from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
-import time
+import time, json, copy
 
 # Configure logging
 logging.basicConfig(
@@ -21,14 +21,38 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 class BPETokenizer:
-    def __init__(self):
-        self.vocab: dict[int, bytes] = {}  # Token ID -> Token Bytes
-        self.merges: list[tuple[bytes, bytes]] = []  # (First Symbols, Second Symbols)
-        self.word_freq: defaultdict[int, int] = defaultdict(int)  # Index -> Word Frequency
-        self.index_word: dict[int, list[bytes]] = {}  # Index -> Words
-        self.pair_ids: defaultdict[tuple[bytes, bytes], set[int]] = defaultdict(set)  # (First Symbols, Second Symbols) -> Set of Indices
-        self.pair_freq: defaultdict[tuple[bytes, bytes], int] = defaultdict(int)  # (First Symbols, Second Symbols) -> Pair Frequency
+    def __init__(
+            self, 
+            vocab: dict[int, bytes] | None = None,
+            merges: list[tuple[bytes, bytes]] | None = None,
+            special_tokens: list[str] = ["<|endoftext|>"],
+            trained: bool = False
+    ) -> None:
+        """
+        Be careful to transfer a default value like "{}" via class methods, because when having multiple objects, 
+        they will share the same content on changeable parameters that have default values.
+        """
+        self.vocab: dict[int, bytes] = vocab if vocab else {}  # Token ID -> Token Bytes
+        self.merges: list[tuple[bytes, bytes]] = merges if merges else [] # (First Symbols, Second Symbols)
+        self.word_to_freq: defaultdict[int, int] = defaultdict(int)  # Index -> Word Frequency
+        self.idx_to_word: dict[int, list[bytes]] = {}  # Index -> Words
+        self.pair_to_ids: defaultdict[tuple[bytes, bytes], set[int]] = defaultdict(set)  # (First Symbols, Second Symbols) -> Set of Indices
+        self.pair_to_freq: defaultdict[tuple[bytes, bytes], int] = defaultdict(int)  # (First Symbols, Second Symbols) -> Pair Frequency
         self.PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""  # Regex pattern for pre-tokenization
+        self.special_tokens = special_tokens
+
+        if trained:
+            self.idx_to_token = vocab
+            self.token_to_idx = {tok: idx for idx, tok in vocab.items()}
+            for sp_tok in special_tokens:
+                sp_tok.encode("utf-8")
+                if sp_tok not in self.token_to_idx:
+                    idx = len(self.idx_to_token)
+                    self.idx_to_token[idx] = sp_tok
+                    self.token_to_idx[sp_tok] = idx
+                    self.vocab[idx] = sp_tok
+            self.merges_set = set(self.merges)
+
 
 
     @staticmethod
@@ -104,8 +128,8 @@ class BPETokenizer:
             idx: int,
             freq: int,
     ) -> None:
-        self.pair_freq[pair] += freq
-        self.pair_ids[pair].add(idx)
+        self.pair_to_freq[pair] += freq
+        self.pair_to_ids[pair].add(idx)
 
 
     def _remove_pair(
@@ -114,12 +138,12 @@ class BPETokenizer:
             idx: int,
             freq: int,
     ) -> None:
-        self.pair_freq[pair] -= freq
-        if self.pair_freq[pair] <= 0:
-            self.pair_freq.pop(pair, None)
-            self.pair_ids.pop(pair, None)
+        self.pair_to_freq[pair] -= freq
+        if self.pair_to_freq[pair] <= 0:
+            self.pair_to_freq.pop(pair, None)
+            self.pair_to_ids.pop(pair, None)
         else:
-            self.pair_ids[pair].discard(idx)
+            self.pair_to_ids[pair].discard(idx)
 
 
     def train(
@@ -160,18 +184,18 @@ class BPETokenizer:
                 enumerate(word_count.items()),
                 total=len(word_count)
             ):
-            self.index_word[i] = [bytes([b]) for b in word.encode('utf-8')]
-            self.word_freq[i] = freq
+            self.idx_to_word[i] = [bytes([b]) for b in word.encode('utf-8')]
+            self.word_to_freq[i] = freq
         logger.info(f'Pre-tokens frequency dictionary built in {time.time() - start_time:.2f} seconds.')
         
         # 2) initial pair frequency & pair to ids
         logger.info("Initial pair frequency calculation started...")
         start_time = time.time()
         for idx, word in tqdm(
-                self.index_word.items(),
-                total=len(self.index_word),
+                self.idx_to_word.items(),
+                total=len(self.idx_to_word),
             ):  # Convert word to bytes and calculate pairs
-            freq = self.word_freq[idx]
+            freq = self.word_to_freq[idx]
             for i in range(len(word) - 1):
                 pair = (word[i], word[i + 1])
                 self._add_pair(pair, idx, freq)
@@ -186,18 +210,17 @@ class BPETokenizer:
             # choose lexicographically greater one.
             # But the lexicographical order is decided by the first pair and then the second pair, 
             # rather than the concatenation of two pairs!!!
-            if not self.pair_freq:
+            if not self.pair_to_freq:
                 break
-            pair = max(self.pair_freq, key=lambda x: (self.pair_freq[x], x))  
-            a = self.pair_freq[pair]
+            pair = max(self.pair_to_freq, key=lambda x: (self.pair_to_freq[x], x))  
             self.merges.append(pair)
             symbol_a, symbol_b = pair
             new_symbol = symbol_a + symbol_b
-            affected_ids = list(self.pair_ids.get(pair, set()))  # Get affected indices
+            affected_ids = list(self.pair_to_ids.get(pair, set()))  # Get affected indices
 
             for idx in affected_ids:
-                word = self.index_word[idx]
-                freq = self.word_freq[idx]
+                word = self.idx_to_word[idx]
+                freq = self.word_to_freq[idx]
                 new_word = []  # New word after merging
                 i = 0
                 while i < len(word) - 1:
@@ -207,14 +230,13 @@ class BPETokenizer:
                     else:
                         new_word.append(word[i])
                         i += 1
-                if i == len(word) - 1:
-                    new_word.append(word[-1])  # Append the last symbol
+                new_word += word[i:]  # Append the last symbol
                 # Operate removal and addition separately to avoid unexpected errors
                 for j in range(len(word) - 1):  # Remove old pairs
                     self._remove_pair((word[j], word[j + 1]), idx, freq)
                 for j in range(len(new_word) - 1):  # Add new pairs
                     self._add_pair((new_word[j], new_word[j + 1]), idx, freq)
-                self.index_word[idx] = new_word
+                self.idx_to_word[idx] = new_word
         logger.info(f"Merging pairs completed in {time.time() - start_time:.2f} seconds.")
 
         # 4) build vocabulary
@@ -223,4 +245,68 @@ class BPETokenizer:
         self.vocab.update({i + len(special_tokens) + 256: pair[0] + pair[1] for i, pair in enumerate(self.merges)})
         logger.info("Vocabulary built successfully.")
 
+        # Init other variables for futher encode & decode
+        self.special_tokens = special_tokens
+        self.idx_to_token = self.vocab
+        self.token_to_idx = {tok: idx for idx, tok in self.vocab.items()}
+        self.merges_set = set(self.merges)
+
         return self.vocab, self.merges
+
+
+    @classmethod
+    def init_by_files(
+            cls, 
+            vocab_filepath: str | os.PathLike, 
+            merges_filepath: str | os.PathLike, 
+            special_tokens: list[str] = ["<|endoftext|>"]
+        ) -> "BPETokenizer":
+        """
+        Using vocab file & merges file to initial a bpe tokenizer
+        """
+        with open(vocab_filepath, "r", encoding="utf-8") as f:
+            token_to_idx = json.loads(f)
+        vocab = {}
+        for token, idx in token_to_idx.items():
+            vocab = {idx, token.encode("utf-8")}
+        merges = []
+        with open(merges_filepath, "r", encoding="utf-8") as f:
+            for line in f:
+                a, b = line.strip().split(" ")
+                merges.append((a.encode("utf-8"), b.encode("utf-8")))
+
+        return cls(vocab, merges, special_tokens=special_tokens, trained=True)
+    
+
+    def encode(self, text: str) -> list[int]:
+        # pre-tokenizaiton
+        b_words = []
+        for match in re.finditer(self.PAT, text):
+            word = match.group(0)   
+            if word in self.special_tokens:
+                b_words.append([word.encode('utf-8')])
+            b_words.append([bytes[b] for b in word.encode('utf-8')])
+        
+        # merge
+        for w in b_words:
+            i = 0
+            while i < len(w) - 1:
+                if (w[i], w[i + 1]) in self.merges_set:
+                    w[i:i+2] = [w[i] + w[i + 1]]
+                    i = max(i - 1, 0)  # At most affect the previous one
+                else:
+                    i += 1
+        
+        return [self.token_to_idx[tok] for w in b_words for tok in w]
+    
+
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for text in iterable:
+            for idx in self.encode(text):
+                yield idx
+    
+
+    def decode(self, ids: list[int]) -> str:
+        tokens = [self.idx_to_token[idx] for idx in ids]
+        b_text = b"".join(tokens)
+        return b_text.decode('utf-8', errors='replace')
